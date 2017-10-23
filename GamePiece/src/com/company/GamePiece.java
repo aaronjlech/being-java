495fe9669d3366974532a6ee9d53069d79345954
Aaron LeCheminant <aaronjlech@gmail.com>
2017-10-23 14:39:47 -0400

    let the games begin

---
+++ GamePiece/src/com/company/GamePiece.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class GamePiece {
+
+    int posX;
+    int posY;
+    boolean frozen;
+    String pieceName;
+
+    public GamePiece(int x, int y, String pieceName) {
+        this.posX = x;
+        this.posY = y;
+        this.frozen = false;
+        this.pieceName = pieceName;
+    }
+    public void move() {
+        if(!this.frozen){
+            this.posX += 1;
+            this.posY += 1;
+        }
+    }
+    public void freeze() {
+        this.freeze = !this.freeze;
+    }
+
+    public void
+}
---
+++ GamePiece/src/com/company/Grid.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Grid {
+    int height;
+    int width;
+    public Grid(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+}
